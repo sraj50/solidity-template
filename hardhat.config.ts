fb7e0745f6ac780c649f0511fa28b55776fc8773
Sid Raj <siddharth.rajesh13@gmail.com>
2021-05-29 22:28:56 +1000

    upgraded to solidity 0.8.0

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -46,7 +46,7 @@ module.exports = {
     apiKey: process.env.ETHERSCAN_API_KEY,
   },
   solidity: {
-    version: "0.7.3",
+    version: "0.8.0",
     settings: {
       optimizer: {
         enabled: true,
